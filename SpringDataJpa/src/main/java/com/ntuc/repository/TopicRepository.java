c6c8ccf4935a3a210abe4abc9b59ef8e2b64b917
sridharguzzu <75741742+sridharguzzu@users.noreply.github.com>
2021-02-16 13:11:28 +0530

    Delete SpringDataJpa directory

--- SpringDataJpa/src/main/java/com/ntuc/repository/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ntuc.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ntuc.model.Topic;
-
-public interface TopicRepository extends CrudRepository<Topic, String>{
-
-}
